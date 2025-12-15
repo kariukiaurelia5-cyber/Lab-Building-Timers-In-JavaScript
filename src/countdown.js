b6fd7a8f7e55ea9002e0b6eb3819589595085c68
Aurelia Kariuki <kariukiaurelia5@gmail.com>
2025-12-15 19:20:05 +0300

    Implement timer functions for countdown, reminder, and recurring timer

--- src/countdown.js
+++ src/countdown.js
@@ -17,6 +17,21 @@ function countdownTimer(startTime, interval) {
   // Log the remaining time and decrement it
   // Stop the timer when time reaches 0
   // Return the timer ID for validation
+  
+  let remainingTime = startTime;
+
+  const timerId = setInterval(() => {
+    console.log(remainingTime);
+
+    if (remainingTime === 0) {
+      clearInterval(timerId);
+    }
+
+    remainingTime--;
+  }, interval);
+
+  return timerId;
 }
+
 
 module.exports = { countdownTimer };
